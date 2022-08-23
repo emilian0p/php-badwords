ec5631211a0afea9abded5f0f21f92dd36e072a9
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-08-23 09:48:18 +0200

    indentation

--- index.php
+++ index.php
@@ -10,20 +10,20 @@
     <h1>Badwords</h1>
 
     <?php
-    $text = "Società Sportiva Lazio, commonly referred to as Lazio, is an Italian professional sports club based in Rome, most known for its football activity. The society, founded in 1900, plays in the Serie A and have spent most of their history in the top tier of Italian football. Lazio have been Italian champions twice (1974, 2000), and have won the Coppa Italia seven times, the Supercoppa Italiana three times, and both the UEFA Cup Winners' Cup and UEFA Super Cup on one occasion.";
-    $lenght = strlen($text);
-    $badword = $_GET["badword"];
-    $badwordReplace = str_replace($badword, "***", $text);
-    $badwordReplaceLenght = strlen($badwordReplace);
+        $text = "Società Sportiva Lazio, commonly referred to as Lazio, is an Italian professional sports club based in Rome, most known for its football activity. The society, founded in 1900, plays in the Serie A and have spent most of their history in the top tier of Italian football. Lazio have been Italian champions twice (1974, 2000), and have won the Coppa Italia seven times, the Supercoppa Italiana three times, and both the UEFA Cup Winners' Cup and UEFA Super Cup on one occasion.";
+        $lenght = strlen($text);
+        $badword = $_GET["badword"];
+        $badwordReplace = str_replace($badword, "***", $text);
+        $badwordReplaceLenght = strlen($badwordReplace);
     ?>
 
     <p>
         <?php
-        echo $text;
+            echo $text;
         ?>
         <span>
             <?php
-            echo $lenght;
+                echo $lenght;
             ?>
         </span>
     </p>
@@ -32,11 +32,11 @@
 
     <p>
         <?php
-        echo $badwordReplace;
+            echo $badwordReplace;
         ?>
         <span>
             <?php
-            echo $badwordReplaceLenght;
+                echo $badwordReplaceLenght;
             ?>
         </span>
     </p>
